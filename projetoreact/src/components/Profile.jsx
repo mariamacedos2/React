8aef7a903c8e1c20dbf4018f096043b4d3ebebc5
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2025-08-29 11:50:22 -0300

    001

---
+++ projetoreact/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+function Profile(){
+
+    return(
+      <div className="profile">
+          <img
+            src="https://github.com/mariamacedos2.png"
+            alt="avatar do Rafael"
+            className="profile-avatar"
+          />
+          <h1 className="profile-name">Maria Macedo</h1>
+          <p className="profile-bio">Desenvolvedor React JS</p>
+        </div>
+    )
+}
+
+export default Profile
