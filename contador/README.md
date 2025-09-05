471079d75975e1fe7c4dcbc0a258cf71ab1c90c2
Maria Eduarda <163424987+mariamacedos2@users.noreply.github.com>
2025-09-05 11:58:36 -0300

    001

---
+++ contador/README.md
@@ -0,0 +1,3 @@
+
+
+# extenção Prettier - code formatter
---
+++ contador/src/components/Contador.jsx
@@ -0,0 +1,18 @@
+import styles from "./Contador.module.css"
+
+export default function Contador(){
+  console.log(styles)
+  return(
+    <>
+      <div className={styles.container}> 
+        <h1>Contador</h1>
+        <div className={styles.contadorDisplay}>0</div>
+        <div className={styles.botaoContainer}>
+          <button>-</button>
+          <button>+</button>
+        </div>
+        <button> Reset</button>
+      </div>
+    </>
+  )
+}
